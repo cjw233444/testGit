2eef7f097128984ac24640c9c7d2254efc3ed0a9
cjw233444 <3182647428@qq.com>
2022-11-05 17:05:36 +0800

    dev分支

--- helloGit/src/main/java/com/cjw/git/HelloGit.java
+++ helloGit/src/main/java/com/cjw/git/HelloGit.java
@@ -3,10 +3,7 @@ package com.cjw.git;
 public class HelloGit {
     public static void main(String[] args){
         System.out.println("Hello Git!");
-        System.out.println("Hello Git!");
-
-
-
+        System.out.println("dev分支");
     }
 
 
